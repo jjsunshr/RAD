50506109d155bacb2054aa2bccbf84fb3c2a415b
Sunqi <jjsunshr@outlook.com>
2021-05-22 09:34:36 +0000

    Top menu and footer, going.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/about'
   get 'customers/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  # root 'application#hello'
   resources :customers
 
   get '/signup', to: 'customers#new'
